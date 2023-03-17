27e4d98b965bdcf4254057385c72fab4a08b51c8
Andrew <andrew@andrewsmith.org>
2023-03-17 14:24:32 -0400

    Create canvas

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Have a snack for energy before you start 🥕");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.style.backgroundColor = "gray";
+
+ctx.fillStyle = "rebeccapurple";
+ctx.fillRect(10, 10, 150, 100);
