a8e45ecbe51b520493275f4ed12a87ec6dd412f4
Andrew <andrew@andrewsmith.org>
2023-03-17 17:16:44 -0400

    Block out main page elements

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Have a snack for energy before you start 🥕");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+canvas.setAttribute("height", getComputedStyle(canvas).height);
+canvas.setAttribute("width", getComputedStyle(canvas).width);
 
-canvas.style.backgroundColor = "gray";
-
-ctx.fillStyle = "rebeccapurple";
+ctx.fillStyle = "#ccc";
 ctx.fillRect(10, 10, 150, 100);
