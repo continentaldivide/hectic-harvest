a3bf089719f5923e9357d29801b3a8e3414835f9
Andrew <andrew@andrewsmith.org>
2023-03-17 17:37:21 -0400

    Set up gameLoop and allow avatar movement with keypress

--- script.js
+++ script.js
@@ -4,5 +4,45 @@ const ctx = canvas.getContext("2d");
 canvas.setAttribute("height", getComputedStyle(canvas).height);
 canvas.setAttribute("width", getComputedStyle(canvas).width);
 
-ctx.fillStyle = "#ccc";
-ctx.fillRect(10, 10, 150, 100);
+const playerCharacter = {
+  x: 10,
+  y: 10,
+  width: 100,
+  height: 100,
+  color: "#ccc",
+  render() {
+    ctx.fillStyle = "#ccc";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  },
+};
+
+document.addEventListener("keydown", handleKeyPressEvent);
+
+function handleKeyPressEvent(e) {
+  const speed = 10;
+  switch (e.key) {
+    case "w":
+    case "ArrowUp":
+      playerCharacter.y -= speed;
+      break;
+    case "s":
+    case "ArrowDown":
+      playerCharacter.y += speed;
+      break;
+    case "a":
+    case "ArrowLeft":
+      playerCharacter.x -= speed;
+      break;
+    case "d":
+    case "ArrowRight":
+      playerCharacter.x += speed;
+      break;
+  }
+}
+
+const gameLoop = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  playerCharacter.render();
+};
+
+const gameLoopInterval = setInterval(gameLoop, 60);
